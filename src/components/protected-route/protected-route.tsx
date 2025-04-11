a7b551163a8bca33642e04f37ef824f4db69a068
DKMFzF <150276575+DKMFzF@users.noreply.github.com>
2025-04-12 03:19:04 +0700

    update(app.tsx): ad docs in file

--- src/components/protected-route/protected-route.tsx
+++ src/components/protected-route/protected-route.tsx
@@ -1,25 +1,42 @@
-// protected-route.tsx
+// import { useSelector } from '../services/store';
 import { Navigate, useLocation } from 'react-router-dom';
-import { FC, ReactElement } from 'react';
+// import { Preloader } from '../components/ui/preloader/preloader';
+// import {
+//   selectUser,
+//   selectIsAuthChecked
+// } from '../services/slices/UserInfoSlice';
 
-interface ProtectedRouteProps {
+type ProtectedRouteProps = {
   onlyUnAuth?: boolean;
-  children: ReactElement;
-}
+  children: React.ReactElement;
+};
 
-export const ProtectedRoute: FC<ProtectedRouteProps> = ({
-  onlyUnAuth = false,
+export const ProtectedRoute = ({
+  onlyUnAuth,
   children
-}) => {
-  const isAuth = true; // Замените на реальную проверку авторизации
+}: ProtectedRouteProps) => {
+  // const isAuthChecked = useSelector(selectIsAuthChecked);
+  // const user = useSelector(selectUser);
   const location = useLocation();
 
-  if (onlyUnAuth && isAuth) {
-    return <Navigate to='/' replace />;
+  // if (!isAuthChecked) {
+  //   // пока идёт чекаут пользователя- показываем прелоадер
+  //   return <Preloader />;
+  // }
+
+  if (!onlyUnAuth) {
+    // если пользователь на странице авторизации и данных в хранилище нет, то делаем редирект
+    return <Navigate replace to='/login' state={{ from: location }} />; // в поле from объекта location.state записываем информацию о URL
   }
 
-  if (!onlyUnAuth && !isAuth) {
-    return <Navigate to='/login' state={{ from: location }} replace />;
+  if (onlyUnAuth) {
+    // если пользователь на странице авторизации и данные есть в хранилище
+    // при обратном редиректе получаем данные о месте назначения редиректа из объекта location.state
+    // в случае если объекта location.state?.from нет — а такое может быть, если мы зашли на страницу логина по прямому URL
+    // мы сами создаём объект c указанием адреса и делаем переадресацию на главную страницу
+    const from = location.state?.from || { pathname: '/' };
+
+    return <Navigate replace to={from} />;
   }
 
   return children;
