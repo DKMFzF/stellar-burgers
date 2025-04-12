87fe142d8c9ee08cf75b44ff847ad019637eba86
DKMFzF <150276575+DKMFzF@users.noreply.github.com>
2025-04-12 22:36:10 +0700

    feat(app-header.tsx): add avtive link function

--- src/components/ui/app-header/app-header.tsx
+++ src/components/ui/app-header/app-header.tsx
@@ -7,29 +7,62 @@ import {
   Logo,
   ProfileIcon
 } from '@zlden/react-developer-burger-ui-components';
+import { Link, useLocation } from 'react-router-dom';
 
-export const AppHeaderUI: FC<TAppHeaderUIProps> = ({ userName }) => (
-  <header className={styles.header}>
-    <nav className={`${styles.menu} p-4`}>
-      <div className={styles.menu_part_left}>
-        <>
-          <BurgerIcon type={'primary'} />
-          <p className='text text_type_main-default ml-2 mr-10'>Конструктор</p>
-        </>
-        <>
-          <ListIcon type={'primary'} />
-          <p className='text text_type_main-default ml-2'>Лента заказов</p>
-        </>
-      </div>
-      <div className={styles.logo}>
-        <Logo className='' />
-      </div>
-      <div className={styles.link_position_last}>
-        <ProfileIcon type={'primary'} />
-        <p className='text text_type_main-default ml-2'>
-          {userName || 'Личный кабинет'}
-        </p>
-      </div>
-    </nav>
-  </header>
-);
+/**
+ * Компонент отрисовки шапки приложения
+ */
+export const AppHeaderUI: FC<TAppHeaderUIProps> = ({ userName }) => {
+  const location = useLocation();
+
+  const isActive = (path: string, exact = false) =>
+    exact ? location.pathname === path : location.pathname.startsWith(path);
+
+  return (
+    <header className={styles.header}>
+      <nav className={`${styles.menu} p-4`}>
+        <div className={styles.menu_part_left}>
+          <Link
+            to='/'
+            className={`${styles.link} ${
+              isActive('/', true) ? styles.link_active : ''
+            }`}
+          >
+            <BurgerIcon type={isActive('/', true) ? 'primary' : 'secondary'} />
+            <p className='text text_type_main-default ml-2 mr-10'>
+              Конструктор
+            </p>
+          </Link>
+          <Link
+            to='/feed'
+            className={`${styles.link} ${
+              isActive('/feed') ? styles.link_active : ''
+            }`}
+          >
+            <ListIcon type={isActive('/feed') ? 'primary' : 'secondary'} />
+            <p className='text text_type_main-default ml-2'>Лента заказов</p>
+          </Link>
+        </div>
+        <div className={styles.logo}>
+          <Link to='/'>
+            <Logo className='' />
+          </Link>
+        </div>
+        <div
+          className={`${styles.link} ${
+            isActive('/profile') ? styles.link_active : ''
+          }`}
+        >
+          <Link to='/profile' className={styles.link_position_last}>
+            <ProfileIcon
+              type={isActive('/profile') ? 'primary' : 'secondary'}
+            />
+            <p className='text text_type_main-default ml-2'>
+              {userName || 'Личный кабинет'}
+            </p>
+          </Link>
+        </div>
+      </nav>
+    </header>
+  );
+};
