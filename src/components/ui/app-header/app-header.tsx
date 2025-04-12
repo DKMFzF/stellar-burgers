134b83594f372f17d71978a844c01ac886650ef4
DKMFzF <150276575+DKMFzF@users.noreply.github.com>
2025-04-13 05:11:03 +0700

    fix(app-header.tsx): fix isActiveLink

--- src/components/ui/app-header/app-header.tsx
+++ src/components/ui/app-header/app-header.tsx
@@ -15,7 +15,7 @@ import { Link, useLocation } from 'react-router-dom';
 export const AppHeaderUI: FC<TAppHeaderUIProps> = ({ userName }) => {
   const location = useLocation();
 
-  const isActive = (path: string, exact = false) =>
+  const isActiveLink = (path: string, exact = false) =>
     exact ? location.pathname === path : location.pathname.startsWith(path);
 
   return (
@@ -25,10 +25,12 @@ export const AppHeaderUI: FC<TAppHeaderUIProps> = ({ userName }) => {
           <Link
             to='/'
             className={`${styles.link} ${
-              isActive('/', true) ? styles.link_active : ''
+              isActiveLink('/', true) ? styles.link_active : ''
             }`}
           >
-            <BurgerIcon type={isActive('/', true) ? 'primary' : 'secondary'} />
+            <BurgerIcon
+              type={isActiveLink('/', true) ? 'primary' : 'secondary'}
+            />
             <p className='text text_type_main-default ml-2 mr-10'>
               Конструктор
             </p>
@@ -36,32 +38,33 @@ export const AppHeaderUI: FC<TAppHeaderUIProps> = ({ userName }) => {
           <Link
             to='/feed'
             className={`${styles.link} ${
-              isActive('/feed') ? styles.link_active : ''
+              isActiveLink('/feed') ? styles.link_active : ''
             }`}
           >
-            <ListIcon type={isActive('/feed') ? 'primary' : 'secondary'} />
+            <ListIcon type={isActiveLink('/feed') ? 'primary' : 'secondary'} />
             <p className='text text_type_main-default ml-2'>Лента заказов</p>
           </Link>
         </div>
+
         <div className={styles.logo}>
           <Link to='/'>
             <Logo className='' />
           </Link>
         </div>
-        <div
-          className={`${styles.link} ${
-            isActive('/profile') ? styles.link_active : ''
+
+        <Link
+          to='/profile'
+          className={`${styles.link} ${styles.link_position_last} ${
+            isActiveLink('/profile') ? styles.link_active : ''
           }`}
         >
-          <Link to='/profile' className={styles.link_position_last}>
-            <ProfileIcon
-              type={isActive('/profile') ? 'primary' : 'secondary'}
-            />
-            <p className='text text_type_main-default ml-2'>
-              {userName || 'Личный кабинет'}
-            </p>
-          </Link>
-        </div>
+          <ProfileIcon
+            type={isActiveLink('/profile') ? 'primary' : 'secondary'}
+          />
+          <p className='text text_type_main-default ml-2'>
+            {userName || 'Личный кабинет'}
+          </p>
+        </Link>
       </nav>
     </header>
   );
