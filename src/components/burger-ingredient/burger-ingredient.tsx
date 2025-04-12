6c4877c47a30b81ec57e3c5abb8eaaa847a7b485
DKMFzF <150276575+DKMFzF@users.noreply.github.com>
2025-04-12 07:18:13 +0700

    feat(burger-ingredient.tsx): added burger element in state

--- src/components/burger-ingredient/burger-ingredient.tsx
+++ src/components/burger-ingredient/burger-ingredient.tsx
@@ -3,12 +3,15 @@ import { useLocation } from 'react-router-dom';
 
 import { BurgerIngredientUI } from '@ui';
 import { TBurgerIngredientProps } from './type';
+import { useDispatch } from 'react-redux';
+import { addIngredientToConstructor } from '../../services/slices/burgers-slice/burgers';
 
 export const BurgerIngredient: FC<TBurgerIngredientProps> = memo(
   ({ ingredient, count }) => {
     const location = useLocation();
+    const dispatch = useDispatch();
 
-    const handleAdd = () => {};
+    const handleAdd = () => dispatch(addIngredientToConstructor(ingredient));
 
     return (
       <BurgerIngredientUI
