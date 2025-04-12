5f411c23e83fa9cde8cc0f88cb412f11cb7887a2
DKMFzF <150276575+DKMFzF@users.noreply.github.com>
2025-04-12 07:00:47 +0700

    feat(ingredient-details.tsx): add function open popup for click burgers

--- src/components/ingredient-details/ingredient-details.tsx
+++ src/components/ingredient-details/ingredient-details.tsx
@@ -1,14 +1,20 @@
 import { FC } from 'react';
 import { Preloader } from '../ui/preloader';
 import { IngredientDetailsUI } from '../ui/ingredient-details';
+import { Params, useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { getIngredientState } from '../../services/slices/ingredients-slice/ingredients';
 
 export const IngredientDetails: FC = () => {
-  /** TODO: взять переменную из стора */
-  const ingredientData = null;
+  const { id } = useParams<Params>();
+  const { ingredients, loading, error } = useSelector(getIngredientState);
 
-  if (!ingredientData) {
-    return <Preloader />;
-  }
+  const ingredientData = ingredients.find((i) => {
+    if (i._id === id) return i;
+  });
+
+  if (!ingredientData || loading) return <Preloader />;
+  if (error) return <Preloader />;
 
   return <IngredientDetailsUI ingredientData={ingredientData} />;
 };
