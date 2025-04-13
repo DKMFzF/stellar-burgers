8b8db1ea78b54d670fcef3db6b0ae97a0c394446
DKMFzF <150276575+DKMFzF@users.noreply.github.com>
2025-04-13 18:50:30 +0700

    docs(burger-ingredient.tsx): added docs in file

--- src/components/burger-ingredient/burger-ingredient.tsx
+++ src/components/burger-ingredient/burger-ingredient.tsx
@@ -1,26 +1,30 @@
 import { FC, memo } from 'react';
 import { useLocation } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 
 import { BurgerIngredientUI } from '@ui';
 import { TBurgerIngredientProps } from './type';
-import { useDispatch } from 'react-redux';
 import { addIngredientToConstructor } from '../../services/slices/burgers-slice/burgers';
 
 /**
- * Комопнент для элемента бургера
+ * Компонент для отображения отдельного ингредиента в списке.
+ * Отвечает за отображение, счётчик и добавление ингредиента в конструктор.
  */
 export const BurgerIngredient: FC<TBurgerIngredientProps> = memo(
   ({ ingredient, count }) => {
     const location = useLocation();
     const dispatch = useDispatch();
 
-    const handleAdd = () => dispatch(addIngredientToConstructor(ingredient));
+    const handleAdd = () => {
+      // Добавляем ингредиент в конструктор
+      dispatch(addIngredientToConstructor(ingredient));
+    };
 
     return (
       <BurgerIngredientUI
         ingredient={ingredient}
         count={count}
-        locationState={{ background: location }}
+        locationState={{ background: location }} // сохраняем состояние для модалки
         handleAdd={handleAdd}
       />
     );
