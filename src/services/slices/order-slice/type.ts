8f49bc5e94fc26522a01b4b54b95f9a7370c7364
DKMFzF <150276575+DKMFzF@users.noreply.github.com>
2025-04-13 20:45:54 +0700

    feat(type.ts): add in types files export default

--- src/services/slices/order-slice/type.ts
+++ src/services/slices/order-slice/type.ts
@@ -2,10 +2,12 @@ import { TOrder } from '@utils-types';
 import { Nullable, StateError } from '../types';
 
 export type OrderNullable = Nullable<TOrder>;
-export type OrderState = {
+type OrderState = {
   orders: TOrder[];
   getOrderByNumberResponse: OrderNullable;
   error: StateError;
   request: boolean;
   responseOrder: OrderNullable;
 };
+
+export default OrderState
