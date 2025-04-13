38bd17a3c7941e6a03aec288ca1ebf7cae4d6a9a
DKMFzF <150276575+DKMFzF@users.noreply.github.com>
2025-04-13 22:36:31 +0700

    feat(register.tsx): added handleSubmit

--- src/pages/register/register.tsx
+++ src/pages/register/register.tsx
@@ -2,19 +2,23 @@ import { FC, SyntheticEvent, useState } from 'react';
 import { RegisterUI } from '@ui-pages';
 import { getRegisterUser } from '../../services/slices/user-Info-slice/user-info';
 import { useDispatch } from '../../services/store';
-import { useNavigate } from 'react-router-dom';
+import { TRegisterData } from '@api';
 
 export const Register: FC = () => {
-  const [userName, setUserName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const navigate = useNavigate();
+  const [userName, setUserName] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
   const dispatch = useDispatch();
 
   const handleSubmit = (e: SyntheticEvent) => {
     e.preventDefault();
-    dispatch(getRegisterUser({ email, password, name: userName }));
-    navigate('/login');
+    const newUserData: TRegisterData = {
+      email,
+      password,
+      name: userName
+    };
+    dispatch(getRegisterUser(newUserData));
   };
 
   return (
